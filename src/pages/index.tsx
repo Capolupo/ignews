cc409fa488a08ca56dd90020d4a059233c8bd2bd
Andre Capolupo <andre.capolupo@gmail.com>
2022-01-26 20:09:34 -0300

    Configuração inicial / Pastas padrão para iniciar o projeto em Next 🚀

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      {/**Jogo Head aqui para cada pagina ter o seu Head ele ira compor o Head do app no Document*/}
+      <Head>
+        <title>Inicio | ig.News</title>
+      </Head>
+      <h1>        
+        olaa <span>Mundo</span>
+      </h1>
+    </>
+  )
+}
